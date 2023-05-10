56b5e488b2b421ee37a430ad5d7c960fc4b77f02
David Buck <github@davidbuck.com>
2023-05-10 12:35:51 +1200

    Tidy up design and code

--- app/page.js
+++ app/page.js
@@ -5,12 +5,12 @@ import { useEffect, useState } from "react";
 
 const Camera = ({ title, src, refreshKey }) => (
   <div>
-    <h3 className="mb-3">{title}</h3>
+    <h3 className="mb-2">{title}</h3>
     <img
       src={`https://www.trafficnz.info/camera/${src}?refreshKey=${refreshKey}`}
       alt={title}
       loading="lazy"
-      className="w-full"
+      className="aspect-video w-full object-cover"
     />
   </div>
 );
@@ -28,13 +28,13 @@ export default function Home() {
 
   return (
     <main className="p-5">
-      <h1 className="text-2xl font-bold">Auckland traffic cameras</h1>
+      <h1 className="mb-4 text-2xl font-bold">Auckland traffic cameras</h1>
       {Cameras.map((section, index) => (
         <div key={index}>
-          <h2 className="font-semibold text-lg sticky top-0 w-full bg-white py-2">
+          <h2 className="sticky top-0 mb-2 w-full bg-white py-2 text-lg font-semibold">
             {section.section}
           </h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 gap-10">
+          <div className="mb-12 grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5">
             {section.images.map((camera, idx) => (
               <Camera
                 key={idx}
