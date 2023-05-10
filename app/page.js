b4532fa8996073f42c7e75137308faa8bd6c6e9c
David Buck <github@davidbuck.com>
2023-05-10 21:10:34 +1200

    Start move to proper api

--- app/page.js
+++ app/page.js
@@ -3,20 +3,21 @@
 import Cameras from "@/json/cameras.json";
 import { useEffect, useState } from "react";
 
-const Camera = ({ title, src, refreshKey }) => (
+const Camera = ({ title, description, src, refreshKey }) => (
   <div>
     <h3 className="mb-2">{title}</h3>
     <img
       src={`https://www.trafficnz.info/camera/${src}?refreshKey=${refreshKey}`}
-      alt={title}
+      alt={description}
       loading="lazy"
-      className="aspect-video w-full object-cover"
+      className="aspect-video w-full rounded object-cover"
     />
   </div>
 );
 
 export default function Home() {
   const [refreshKey, setRefreshKey] = useState(0);
+  const [searchString, setSearchString] = useState("");
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -26,26 +27,59 @@ export default function Home() {
     return () => clearInterval(intervalId);
   }, []);
 
+  const [filteredCameras, setFilteredCameras] = useState(Cameras);
+
+  const handleSearchChange = (event) => {
+    setSearchString(event.target.value);
+    const filtered = Cameras.map((section) => ({
+      ...section,
+      images: section.images.filter(
+        (camera) =>
+          camera.title
+            .toLowerCase()
+            .includes(event.target.value.toLowerCase().trim()) ||
+          camera.description
+            .toLowerCase()
+            .includes(event.target.value.toLowerCase().trim())
+      ),
+    })).filter((section) => section.images.length > 0);
+
+    setFilteredCameras(filtered);
+  };
+
   return (
-    <main className="p-5">
+    <main className="p-5 md:p-10">
       <h1 className="mb-4 text-2xl font-bold">Auckland traffic cameras</h1>
-      {Cameras.map((section, index) => (
-        <div key={index}>
-          <h2 className="sticky top-0 mb-2 w-full bg-white py-2 text-lg font-semibold">
-            {section.section}
-          </h2>
-          <div className="mb-12 grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5">
-            {section.images.map((camera, idx) => (
-              <Camera
-                key={idx}
-                title={camera.title}
-                src={camera.src}
-                refreshKey={refreshKey}
-              />
-            ))}
+      <input
+        type="text"
+        placeholder="Search cameras..."
+        value={searchString}
+        onChange={handleSearchChange}
+        className="mb-4 w-full rounded-md bg-white p-2 ring-1 ring-slate-500 focus:outline-none focus-visible:ring dark:bg-slate-900"
+      />
+      {filteredCameras.length > 0 ? (
+        filteredCameras.map((section, index) => (
+          <div key={index}>
+            <h2 className="sticky top-0 mb-2 w-full bg-white py-2 text-lg font-semibold dark:bg-slate-900 dark:text-white">
+              {section.section}
+            </h2>
+
+            <div className="mb-12 grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+              {section.images.map((camera, idx) => (
+                <Camera
+                  key={idx}
+                  title={camera.title}
+                  description={camera.description}
+                  src={camera.src}
+                  refreshKey={refreshKey}
+                />
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        <>No matches</>
+      )}
     </main>
   );
 }
